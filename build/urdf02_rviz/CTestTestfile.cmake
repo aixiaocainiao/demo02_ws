6bee3775bfc6f480a8db217790cac381e09cad6d
aixiaocainiao <18338451979@163.com>
2023-03-17 19:33:43 +0800

    first commit

---
+++ build/urdf02_rviz/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yunfang/demo02_ws/src/urdf02_rviz
+# Build directory: /home/yunfang/demo02_ws/build/urdf02_rviz
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ devel/share/gennodejs/ros/arbotix_msgs/srv/_index.js
@@ -0,0 +1,14 @@
+
+"use strict";
+
+let Relax = require('./Relax.js')
+let Enable = require('./Enable.js')
+let SetupChannel = require('./SetupChannel.js')
+let SetSpeed = require('./SetSpeed.js')
+
+module.exports = {
+  Relax: Relax,
+  Enable: Enable,
+  SetupChannel: SetupChannel,
+  SetSpeed: SetSpeed,
+};
